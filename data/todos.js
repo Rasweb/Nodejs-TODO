c661ae10418978d9ec7496b37ad687a67621c6f5
RasWeb <rasmuspalm@fastmail.se>
2022-01-31 09:51:29 +0100

    added some template date

--- data/todos.js
+++ data/todos.js
@@ -1,8 +1,23 @@
 const todos = [
-  { id: 1, created: "", Description: "Pay bills", Done: false },
-  { id: 2, created: "", Description: "Train", Done: false },
-  { id: 3, created: "", Description: "Cook dinner", Done: false },
-  { id: 4, created: "", Description: "Read Book", Done: false },
+  {
+    id: 1,
+    created: "2022-01-31 09:49:15",
+    Description: "Pay bills",
+    Done: false,
+  },
+  { id: 2, created: "2022-01-31 09:49:25", Description: "Train", Done: false },
+  {
+    id: 3,
+    created: "2022-01-31 09:49:35",
+    Description: "Cook dinner",
+    Done: false,
+  },
+  {
+    id: 4,
+    created: "2022-01-31 09:49:45",
+    Description: "Read Book",
+    Done: false,
+  },
 ];
 
 module.exports = todos;
